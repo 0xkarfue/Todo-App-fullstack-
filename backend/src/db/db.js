09e65e078666c13a385e72d6a3929bbe4e15abde
0xkarfue <0xkarfue@gmail.com>
2024-11-29 17:55:31 +0530

    seted up the server

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+// import { DB_NAME } from "../constants.js";
  
 
-// const DB_NAME = "todoApp";
+const DB_NAME = "todoApp";
 
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MONGODB connected !! DB: HOST ${connectionInstance.connection.host}`);
-        console.log(`MONGODB connected !! DB: HOST ${connectionInstance}`);
+        // console.log(`MONGODB connected !! DB: HOST ${connectionInstance}`);
         
     } catch (error) {
         console.log('ERROR : ', error);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,23 @@
+import express from 'express';
 import connectDB from "./db/db.js";
 import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+})
 
-connectDB()
+const app = express();
+
+connectDB().then(() => {
+
+    app.listen(process.env.PORT, () => {
+        console.log('server is running.......');
+    })
+}
+).catch((error) => {
+
+    console.log(`ERROR !!! `, error)
+}
+
+)
+
+
